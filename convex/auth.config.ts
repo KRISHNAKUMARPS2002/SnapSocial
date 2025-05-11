c92d55ff83adfb77fe36799314ba90128ebe521c
KRISHNAKUMARPS2002 <krishnakumarkichu2002@gmail.com>
2025-05-11 17:03:17 +0530

    Intial Commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://climbing-oriole-39.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ providers/ClerkAndConvexProvider.tsx
@@ -0,0 +1,30 @@
+import { ClerkLoaded, ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { tokenCache } from "../cache";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error(
+    "EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY is not defined. Please set it in your .env file."
+  );
+}
+
+export default function ClerkAndConvexProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <ClerkLoaded>{children}</ClerkLoaded>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
